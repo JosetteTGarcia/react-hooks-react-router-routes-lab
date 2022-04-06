b7772254d9b6ddea7ca788c976248062684acc8f
Josette Garcia <josettetgarcia@gmail.com>
2022-04-05 23:10:58 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div class= "navbar">
+   <NavLink
+    to = "/"
+    exact
+    activeStyle={{backgroung: "darkblue"}}>
+      Home
+  </NavLink>
+  <NavLink
+    to = "/movies"
+    exact
+    activeStyle={{backgroung: "darkblue"}}>
+      Movies
+  </NavLink>
+  <NavLink
+    to = "/directors"
+    exact
+    activeStyle={{backgroung: "darkblue"}}>
+      Directors
+  </NavLink>
+  <NavLink
+    to = "/actors"
+    exact
+    activeStyle={{backgroung: "darkblue"}}>
+      Actors
+  </NavLink>
+  </div>;
 }
 
 export default NavBar;
